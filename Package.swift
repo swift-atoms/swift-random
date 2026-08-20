c5fa36c06d3bd9e008d2cd247129b7e59899ac5c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:28 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-random-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -21,8 +21,7 @@ let package = Package(
             targets: ["Random Primitives Test Support"]
         ),
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Random Primitives"
@@ -30,7 +29,7 @@ let package = Package(
         .target(
             name: "Random Primitives Test Support",
             dependencies: [
-                "Random Primitives",
+                "Random Primitives"
             ],
             path: "Tests/Support"
         ),
